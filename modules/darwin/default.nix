31c6238df5682ae2a00153708a66282d0280e981
Craig Adams <craigness24@gmail.com>
2023-01-29 16:26:48 -0600

    nesting

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -8,6 +8,7 @@
     systemPath = [ "/opt/homebrew/bin" ];
     pathsToLink = [ "/Applications" ];
   };
+  services.nix-daemon.enable = true;
   nix = {
     package = pkgs.nix;
     gc = {
@@ -20,18 +21,22 @@
       experimental-features = nix-command flakes
     '';
   };
-  system.keyboard.enableKeyMapping = true;
-  system.keyboard.remapCapsLockToEscape = true;
+  system = {
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
+    };
+    defaults = {
+        finder.AppleShowAllExtensions = true;
+        dock.autohide = true;
+        NSGlobalDomain.AppleShowAllExtensions = false;
+        NSGlobalDomain.InitialKeyRepeat = 14;
+        NSGlobalDomain.KeyRepeat = 1;
+    };
+  };
   fonts.fontDir.enable = true; # DANGER
   fonts.fonts = [ (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; }) ];
-  services.nix-daemon.enable = true;
-  system.defaults = {
-    finder.AppleShowAllExtensions = true;
-    dock.autohide = true;
-    NSGlobalDomain.AppleShowAllExtensions = false;
-    NSGlobalDomain.InitialKeyRepeat = 14;
-    NSGlobalDomain.KeyRepeat = 1;
-  };
+
   # backwards compat; don't change
   system.stateVersion = 4;
 }
